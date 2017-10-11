97fe40bf3dbf87ed27e01d2a8e3a10bae783c865
James Cerniglia <cerniglj1@hawkmail.newpaltz.edu>
2017-10-11 13:20:40 -0400

    Major Changes

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+  entry: './jquery-mockup/src/navigation.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'jquery-mockup/dist')
+  }
+};
