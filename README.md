6773ecb7d813a9a00c20d7f18d1f1c0a565c4489
James Cerniglia <cerniglj1@hawkmail.newpaltz.edu>
2017-10-16 17:54:05 -0400

    10/16/17 patch from class

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# 2017FallWebProgramming
-What do you meme
+# 2017fall
+what do you meme
 
 ## This is a game
 Based on Bubble Talk
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,39 @@
-const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const extractSass = new ExtractTextPlugin({
+    filename: "[name].bundle.css",
+    disable: process.env.NODE_ENV === "development"
+});
 
 module.exports = {
- entry: './jquery-mockup/src/navigation.js',
- output: {
-   filename: 'bundle.js',
-   path: path.resolve(__dirname, 'jquery-mockup/dist')
- }
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: extractSass.extract({
+                use: [{
+                    loader: "css-loader"
+                }, {
+                    loader: "sass-loader"
+                }],
+                // use style-loader in development
+                fallback: "style-loader"
+            })
+        },
+        {
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/
+        }]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery',
+            Popper: ['popper.js', 'default'],
+          }),
+        extractSass
+    ],
+    devtool: 'source-map'
 };
