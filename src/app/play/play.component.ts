af300046495815ac593d0b07a2c1cdb217a65f10
James Cerniglia <cerniglj1@hawkmail.newpaltz.edu>
2017-11-10 10:17:30 -0500

    idek

--- src/app/play/play.component.ts
+++ src/app/play/play.component.ts
@@ -1,31 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { Room, Player, Quote } from '../models/game';
+import { Component, OnInit } from "@angular/core";
+import { Room, Player, Quote } from "../models/game";
 
-import * as $ from 'jquery';
-
+import * as $ from "jquery";
 
 @Component({
-  selector: 'app-play',
-  templateUrl: './play.component.html',
-  styleUrls: ['./play.component.scss']
+  selector: "app-play",
+  templateUrl: "./play.component.html",
+  styleUrls: ["./play.component.scss"]
 })
 export class PlayComponent implements OnInit {
+  apiRoot = "//localhost:3001";
+  room = new Room();
+  constructor() {}
 
-    apiRoot = "//localhost:3001"
-    room = new Room();
-    constructor() { }
+  ngOnInit() {
+    setInterval(() => this.update(), 1000);
+  }
 
-    ngOnInit() {
-        setInterval(()=> this.update(), 1000)
-    }
-
-    update(){
-        $.get(this.apiRoot + "/game/room/picture").done( data => {
-            this.room.picture = data;
-        });
-        $.getJSON(this.apiRoot + "/game/room/quotes").done( data =>{
-            this.room.quotes = data;
-        });
-    }
-
+  update() {
+    $.get(this.apiRoot + "/game/room/picture").done(data => {
+      this.room.picture = data;
+    });
+    $.getJSON(this.apiRoot + "/game/room/quotes").done(data => {
+      this.room.quotes = data;
+    });
+  }
 }
