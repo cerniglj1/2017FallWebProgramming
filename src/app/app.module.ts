40139043973032641d041533d393c89e8bc718c8
James Cerniglia <cerniglj1@hawkmail.newpaltz.edu>
2017-11-20 11:15:20 -0500

    11/20/17

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from "@angular/router";
+import { HttpModule } from "@angular/http";
 
 import { AppComponent } from './app.component';
 import { HeadComponent } from './head/head.component';
 import { NavComponent } from './nav/nav.component';
 import { PlayComponent } from './play/play.component';
 import { IndexComponent } from './index/index.component';
+import { GameService } from './models/game.service';
+import { LoginrComponent } from './loginr/loginr.component';
 
 @NgModule({
   declarations: [
@@ -14,17 +17,20 @@ import { IndexComponent } from './index/index.component';
     HeadComponent,
     NavComponent,
     PlayComponent,
-    IndexComponent
+    IndexComponent,
+    LoginrComponent
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     RouterModule.forRoot([
         { path: "play", component: PlayComponent },
         { path: "home", component: IndexComponent },
+        { path: "login", component: LoginrComponent },
         { path: "", pathMatch: "full", redirectTo: "/home" }
     ])
   ],
-  providers: [],
+  providers: [ GameService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
